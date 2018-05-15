e4ad289bcae4f8e1dad351a354bad84cfe4e6d04
Axel Díaz <diaz.axelio@gmail.com>
2018-05-15 18:54:51 -0400

    Fix PEP8 and import

--- versionfield/__init__.py
+++ versionfield/__init__.py
@@ -1,7 +1,7 @@
 from django.db import models
 from .constants import DEFAULT_NUMBER_BITS
 from .version import Version
-from utils import convert_version_string_to_int, convert_version_int_to_string
+from .utils import convert_version_string_to_int, convert_version_int_to_string
 import forms
 
 
